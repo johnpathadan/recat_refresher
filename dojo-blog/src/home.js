ba9ecec6897973d69db9ab71ad1d248db808b4f9
johnpathadan <johnpathadan@gmail.com>
2022-09-28 14:24:16 +0530

    fix: day 02 begins

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,17 +1,23 @@
+import { useState } from "react";
 const Home = () => {
-  const handleClick = (e) => {
-    console.log("Hello Coffee Lovers", e);
-  };
-  const handleClickAgain = (name, e) => {
-    console.log(`Hello ${name}`, e.target);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new Website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome Party", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "yoda",
+      id: 3,
+    },
+  ]);
   return (
     <div className="homePage">
-      <h2>Home Page</h2>
-      <button onClick={handleClick}>Click Me</button>
-      <button onClick={(e) => handleClickAgain("Mario", e)}>
-        Click Me Again
-      </button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written By {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
