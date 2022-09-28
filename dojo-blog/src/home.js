720434d894d2e84cbd812fd0262066e4fb4af5eb
johnpathadan <johnpathadan@gmail.com>
2022-09-28 14:41:06 +0530

    wednesday evening commit

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./Bloglist";
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "My new Website", body: "lorem ipsum...", author: "mario", id: 1 },
@@ -12,12 +13,7 @@ const Home = () => {
   ]);
   return (
     <div className="homePage">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written By {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 };
